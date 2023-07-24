fe6bd4000f75cb1020861648488e85adefa5e5a4
Dmitriy-Romanchuk <112806589+Dmitriy-Romanchuk@users.noreply.github.com>
2023-07-24 17:57:13 +0300

    Updated tests

--- tests/test_multiply.cpp
+++ tests/test_multiply.cpp
@@ -4,7 +4,6 @@
 
 int main()
 {
-
     mat3 first_matrix = {5, 0, 0,
                          0, 5, 0,
                          0, 0, 5};
@@ -16,11 +15,10 @@ int main()
     mat3 reference = {25, 0, 0,
                       0, 25, 0,
                       0, 0, 25};
-    mat3 tester;
 
-    tester.multiply(first_matrix, second_matrix);
+    mat3 tester = mat3::multiply(first_matrix, second_matrix);
 
-    if(tester == reference)
+    if (tester == reference)
         return EXIT_SUCCESS;
     else
         return EXIT_FAILURE;
---
+++ tests/test_read_from_file.cpp
@@ -0,0 +1,15 @@
+#include <class_mat3.h>
+#include "helpers.h"
+
+#include <iostream>
+#include <string>
+
+std::string output = "output.txt";
+
+mat3 reference { 1, 2, 3,
+                 4, 5, 6,
+                 7, 8, 9 };
+
+
+
+
