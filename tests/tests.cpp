4b0ff40896e1fe46d67727d8fc55f1a11f411ff7
Dmitriy-Romanchuk <112806589+Dmitriy-Romanchuk@users.noreply.github.com>
2023-08-01 18:22:29 +0300

    updated tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,20 +1,27 @@
 #include "simple_test.hpp"
 
 #include <cstdlib>
+#include <iostream>
+#include <cstring>
 
-int test_1([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+int test_1([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
-    return EXIT_SUCCESS;
+    if (strcmp(argv[0], "success") == 0)
+    {
+        return EXIT_SUCCESS;
+    }
+
+    return EXIT_FAILURE;
 }
 
-int test_2([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+int test_2([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
     return EXIT_FAILURE;
 }
 
 void initializeTests()
 {
-    auto& tests = TestStorage::instance();
+    auto &tests = TestStorage::instance();
 
     tests.addTest("test_1", &test_1);
     tests.addTest("test_2", &test_2);
