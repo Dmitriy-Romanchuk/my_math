58a22a266d3c6120c778f115dcaab85dfbf3912f
Dmitriy-Romanchuk <roman4ukdimka@mail.ru>
2023-08-01 23:53:54 +0300

    added my custom tests into tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,28 +1,78 @@
+#include "class_mat3.h"
 #include "simple_test.hpp"
 
 #include <cstdlib>
 #include <iostream>
 #include <cstring>
 
-int test_1([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
+int test_multiply([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
-    if (strcmp(argv[0], "success") == 0)
-    {
+    mat3 first_matrix = {5, 0, 0,
+                         0, 5, 0,
+                         0, 0, 5};
+
+    mat3 second_matrix = {5, 0, 0,
+                          0, 5, 0,
+                          0, 0, 5};
+
+    mat3 expected = {25, 0, 0,
+                      0, 25, 0,
+                      0, 0, 25};
+
+    mat3 current = mat3::multiply(first_matrix, second_matrix);
+
+    if (current == expected)
         return EXIT_SUCCESS;
-    }
-
-    return EXIT_FAILURE;
+    else
+        return EXIT_FAILURE;
 }
 
-int test_2([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
+int test_equal_operator([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
-    return EXIT_FAILURE;
+    mat3 first_matrix = {5, 0, 0,
+                         0, 5, 0,
+                         0, 0, 5};
+
+    mat3 second_matrix = {5, 0, 0,
+                          0, 5, 0,
+                          0, 0, 5};
+
+    if (first_matrix == second_matrix)
+        return EXIT_SUCCESS;
+    else
+        return EXIT_FAILURE;
 }
+
+int test_no_equal_operator([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
+{
+    mat3 first_matrix = {10, 0, 0,
+                         0, 10, 0,
+                         0, 0, 10};
+
+    mat3 second_matrix = {5, 0, 0,
+                          0, 5, 0,
+                          0, 0, 5};
+
+    if (first_matrix != second_matrix)
+        return EXIT_SUCCESS;
+    else
+        return EXIT_FAILURE;
+}
+
+int test_read_from_file([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
+{
+    
+    
+    return EXIT_SUCCESS;
+}
+
 
 void initializeTests()
 {
     auto &tests = TestStorage::instance();
 
-    tests.addTest("test_1", &test_1);
-    tests.addTest("test_2", &test_2);
+    tests.addTest("test_multiply", &test_multiply);
+    tests.addTest("test_equal_operator", &test_equal_operator);
+    tests.addTest("test_no_equal_operator", &test_no_equal_operator);
+    tests.addTest("test_read_from_file", &test_read_from_file);
 }
