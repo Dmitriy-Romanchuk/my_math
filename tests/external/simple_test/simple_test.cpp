f0a942c21a9bcc1bdf61e209bd020ca934706bbd
Dmitriy-Romanchuk <112806589+Dmitriy-Romanchuk@users.noreply.github.com>
2023-08-01 17:54:18 +0300

    updated tests

--- tests/external/simple_test/simple_test.cpp
+++ tests/external/simple_test/simple_test.cpp
@@ -1,23 +1,23 @@
-#include"simple_test.hpp"
+#include "simple_test.hpp"
 
 #include <cassert>
 #include <cstdlib>
 
-TestStorage& TestStorage::instance()
+TestStorage &TestStorage::instance()
 {
     static TestStorage storage;
     return storage;
 }
 
-void TestStorage::addTest(const std::string& name, TestFunction func)
+void TestStorage::addTest(const std::string &name, TestFunction func)
 {
-//if (auto result = m_tests.try_emplace(name, func); result.second == false)
-//{
-//    assert(!"m_tests already contain this test");
-//}
+    if (auto result = m_tests.try_emplace(name, func); result.second == false)
+    {
+        assert(!"m_tests already contain this test");
+    }
 }
 
-int TestStorage::runTest(const std::string& name, int argc, char* argv[]) const
+int TestStorage::runTest(const std::string &name, int argc, char *argv[]) const
 {
     if (auto it = m_tests.find(name); it != m_tests.end())
     {
@@ -28,12 +28,12 @@ int TestStorage::runTest(const std::string& name, int argc, char* argv[]) const
     return EXIT_FAILURE;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     assert((argc > 1 && "fail arg count"));
 
     initializeTests();
 
-    auto& tests = TestStorage::instance();
+    auto &tests = TestStorage::instance();
     return tests.runTest(argv[1], argc - 2, argv + 2);
 }
