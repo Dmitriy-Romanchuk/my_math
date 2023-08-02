bd3dbd6fb3282d994fccc0da0e2a0821524de129
Mixail <mixail.any@gmail.com>
2023-08-03 02:52:08 +0300

    Split String

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -24,8 +24,6 @@ int test_multiply([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 
     mat3 current = mat3::multiply(first_matrix, second_matrix);
 
-    std::cout << "test Dima" << std::endl;
-
     return (current == expected) == EXIT_SUCCESS;
 }
 
@@ -57,17 +55,15 @@ int test_no_equal_operator([[maybe_unused]] int argc, [[maybe_unused]] char *arg
 
 int test_read_from_file([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
-   
-    const char* path = "D:\\learning\\C++\\mat3\\tests\\output_matrix.txt"; // TO DO change default path
+    const char *path = "output_matrix.txt"; // TO DO change default path
 
     mat3 first_matrix = {10, 0, 0,
                          0, 10, 0,
                          0, 0, 10};
 
     write_to_file(path, first_matrix);
-
     std::vector<int> init = read_from_file(path);
-    
+
     mat3 second_matrix(init);
     second_matrix.show();
 
